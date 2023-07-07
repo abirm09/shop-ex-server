b7b5f1b1e6eaf2ce2d91f364f8f0ecffea0596fd
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-07-07 23:01:38 +0600

    implemented srore user data api

--- index.js
+++ index.js
@@ -15,3 +15,47 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is started at port ${port}`);
 });
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    const usersCollection = client.db("shop-ex").collection("users");
+    //APIs are started here
+    // store user data to db
+    app.post("/store-user", async (req, res) => {
+      const { name, profilePic, profileDelete, email } = req.body;
+      const userInfo = {
+        name,
+        profilePic,
+        profileDelete,
+        email,
+        openingData: new Date(),
+        role: "user",
+      };
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
